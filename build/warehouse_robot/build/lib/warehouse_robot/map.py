496ff92692542405f84acf0de473227c54838e74
varun kashyap tv <varunkashyaptv@gmail.com>
2026-08-29 01:01:20 +0530

    Remove sensitive .env and log files, update gitignore

--- build/warehouse_robot/build/lib/warehouse_robot/map.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-from matplotlib import pyplot as plt
-import os
-
-os.path.expanduser("~/map.pgm")
-data = Image.open(os.path.expanduser("~/map.pgm"))
-plt.imshow(data)
-plt.show()
